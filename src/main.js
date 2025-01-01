e9a89552d8892820c408d97eb2da51e7904d783a
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2025-01-01 19:54:56 -0400

    use jsr:@nelreina/redis-client

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import "https://deno.land/x/logging@v2.0.0/mod.ts";
 import { Hono } from "npm:hono";
 import { logger } from "npm:hono/logger";
 import { connectToPocketbase } from "./config/pocketbase.js";
-import { connectToEventStream } from "./config/redis-client.js";
+import { client as redis } from "./config/redis-client.js";
 import eventHandler from "./lib/event-handler.js";
 import schedule from "npm:node-schedule";
 
@@ -25,7 +25,7 @@ if (await connectToPocketbase()) {
   schedule.scheduleJob("*/5 * * * * *", () => {
     console.info("The answer to life, the universe, and everything!");
   });
-  await connectToEventStream(STREAM, eventHandler, false);
+  await redis.connectToEventStream(STREAM, eventHandler, false);
   Deno.serve({ port: PORT }, app.fetch);
   console.info(`🚀 ${SERVICE_NAME} is running on http://localhost:${PORT}`);
 } else {
