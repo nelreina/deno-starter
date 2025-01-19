61f4217889aaef64d80a749df597435db3a084bc
Nelson Reina (@nelreina) <nelreina@gmail.com>
2025-01-18 20:37:29 -0400

    pass container name or service name as consumer name
    upgrade redis client for consumer name option

--- src/config/redis-client.js
+++ src/config/redis-client.js
@@ -1,12 +1,13 @@
 import "https://deno.land/x/logging@v2.0.0/mod.ts";
 import { RedisClient } from "@nelreina/redis-client";
-
+import { getRunningContainerName } from "./docker-info.js";
+const SERVICE_NAME = Deno.env.get("SERVICE_NAME");
 export const client = new RedisClient({
   redisHost: Deno.env.get("REDIS_HOST"),
   redisPort: Deno.env.get("REDIS_PORT"),
   redisUser: Deno.env.get("REDIS_USER"),
   redisPw: Deno.env.get("REDIS_PW"),
-  serviceName: Deno.env.get("SERVICE_NAME"),
-  timeZone: Deno.env.get("TIME_ZONE"),
-  streamMaxLength: Deno.env.get("STREAM_MAX_LENGTH") || 10000,
+  serviceName: SERVICE_NAME,
+  timeZone: Deno.env.get("TIMEZONE"),
+  streamConsumerName: await getRunningContainerName() || SERVICE_NAME.toLocaleLowerCase(),
 });
